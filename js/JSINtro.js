e577edf878950d06aa02d68d0ba63b0b26ee3451
Ryan Kendall <ryan.morris.kendall@gmail.com>
2020-08-10 14:49:18 -0500

    working with prompts

--- js/JSINtro.js
+++ js/JSINtro.js
@@ -2,6 +2,18 @@
 
 console.log("Still works!");
 
-surprise = "How copy?";
+var surprise = "How copy?";
 
-console.log(surprise);
+alert(surprise);
+
+var queryMIB = "Have you been probed by aliens?";
+confirm(queryMIB);
+console.log("answer="+queryMIB);
+
+var queryFollowUp = "What color were the aliens?";
+var responseFollowUp = prompt(queryFollowUp);
+console.log(responseFollowUp);
+
+var queryFinal = "Saucer or transporter?";
+var answerFinal = prompt(queryFinal);
+console.log(typeof answerFinal);
